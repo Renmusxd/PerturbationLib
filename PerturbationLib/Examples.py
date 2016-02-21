b4ca196d9c5f9ed78be7189e3935a32100c43f69
Renmusxd <sumnernh@gmail.com>
2016-02-20 19:36:17 -0500

    Some examples

--- PerturbationLib/Examples.py
+++ PerturbationLib/Examples.py
@@ -7,4 +7,11 @@ def A():
     c = SingleState([0,1])
     d = a+b+c+c
     e = d.transpose() * c
-    f = ( XOp(1) * c)
+    assert e==2
+    f = ( AOp(1) * c)
+    g = f.transpose() * SingleState([0,0])
+    assert g==1
+
+def B():
+    d = ( XOp(1) * SingleState([1,0]) )
+    e = ( AOp(1) * d ) + ( COp(1) * d )
